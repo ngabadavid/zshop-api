c0469a00dd7ba116e430985e97863fa3116a2e97
ngabadavid <107659284+ngabadavid@users.noreply.github.com>
2023-05-26 09:40:08 -0400

    Successfully implemented Miscroservice communication(Requests/Reply) threw a kafka topic with ReplyingKafkaTemplate. A lot of code cleaning is required. But it wooooooorrrrrrkkk !!! :)

---
+++ zshop-auth/src/main/java/com/davidngaba/zshopauth/ZshopAuthApplication.java
@@ -0,0 +1,67 @@
+package com.davidngaba.zshopauth;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.jdbc.datasource.AbstractDataSource;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@SpringBootApplication
+public class ZshopAuthApplication {
+	@Order(Ordered.HIGHEST_PRECEDENCE)
+	private class RetryableDataSourceBeanPostProcessor implements BeanPostProcessor {
+		@Override
+		public Object postProcessBeforeInitialization(Object bean, String beanName)
+				throws BeansException {
+			if (bean instanceof DataSource) {
+				bean = new RetryableDataSource((DataSource)bean);
+			}
+			return bean;
+		}
+
+		@Override
+		public Object postProcessAfterInitialization(Object bean, String beanName)
+				throws BeansException {
+			return bean;
+		}
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(ZshopAuthApplication.class, args);
+	}
+
+	@Bean
+	public BeanPostProcessor dataSouceWrapper() {
+		return new RetryableDataSourceBeanPostProcessor();
+	}
+}
+
+class RetryableDataSource extends AbstractDataSource {
+
+	private DataSource delegate;
+
+	public RetryableDataSource(DataSource delegate) {
+		this.delegate = delegate;
+	}
+
+	@Override
+	@Retryable(maxAttempts=10, backoff=@Backoff(multiplier=2.3, maxDelay=30000))
+	public Connection getConnection() throws SQLException {
+		return delegate.getConnection();
+	}
+
+	@Override
+	@Retryable(maxAttempts=10, backoff=@Backoff(multiplier=2.3, maxDelay=30000))
+	public Connection getConnection(String username, String password)
+			throws SQLException {
+		return delegate.getConnection(username, password);
+	}
+}
---
+++ zshop-auth/src/test/java/com/davidngaba/zshopauth/ZshopAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidngaba.zshopauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZshopAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
